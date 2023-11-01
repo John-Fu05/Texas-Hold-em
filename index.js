a11246a5c6e116352dfdd2d35e44b9127ffbd71e
John-Fu05 <tianlin.fu05@gmail.com>
2023-10-31 17:54:36 -0700

    organized code

--- index.js
+++ index.js
@@ -4,136 +4,195 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-var stage = 0
-var first_hand = ['', '']
-var second_hand = ['', '']
-var flop = ['', '', '', '', '']
-var turn = ['']
-var river = ['']
-//temp array for each game
-var temp = []
-
-var ready_index = 0;
-
-
-var IDs = []
-
-const deck = ['AH', ' 2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
-              'AS', ' 2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS',
-              'AD', ' 2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
-              'AC', ' 2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC']
+const User = require('./user.js')
 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static('./images'));
+
+var stage = 0
+var flop = ['', '', '']
+var turn = ['']
+var river = ['']
+var temp = []; // temp array for each game
+var total_pot =0; // pot in the room
+var check_index = 0;
+var ready_index = 0;
+var counter = 0;
+var IDs = [];
+var fold_list =[];
+
+// s --> heart
+// l --> dime
+// p --> spade
+// k --> club
+const deck = ['sa', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk',
+              'pa', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'pj', 'pq', 'pk',
+              'la', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7', 'l8', 'l9', 'l10', 'lj', 'lq', 'lk',
+              'ka', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'k8', 'k9', 'k10', 'kj', 'kq', 'kk']
+
 
 io.on('connection', (socket) => {
-  ///////
-  socket.on('message_container', () => {
-    io.emit('message_container', socket.id);
-  });
-  ////
-  //console.log(socket.id);
-  console.log('a user connected');
-  IDs.push(socket.id);
-  
-
+  const a = new User(socket.id);
+  console.log('user    ' + socket.id + '    connected');
+  IDs.push(a);
+  io.to(a.ID).emit('ID message', a.ID);
+  //when user disconnect
   socket.on('disconnect', () => {
-    console.log('user disconnected');
-    IDs.pop();
+    //reset everything 
+    console.log('user    ' + socket.id + '    disconnected');
+    IDs = IDs.filter(User => User.ID != socket.id);
+    resetOnServer();
+    io.emit('reset game');
   });
 });
 
+
 io.on('connection', (socket) => {
+
+  //when user check/bet increase counter
+  socket.on('counter message', ()=> {
+    counter++;
+  })
+
+  //when user fold
+  socket.on('fold message', (arg)=> {
+    fold_list.push(arg);
+  })
+
+  socket.on('check message', ()=> {
+    
+    // ———————————————目前程序正常使用———————————————————————————————————————————//
+    // fold 逻辑有问题 到底在哪check_index++
+    //html中到底先 check message 还是先fold message
+    // enable message when on turn
+    //fold本质-- fold 之后发送check message（对于本轮）， 发送给服务器id 组成list
+    //检查目前序列中这个人在不在list中 在的话跳过到下一个人（几个人）
+    // if(!fold_list.includes(IDs[counter % IDs.length].ID)){
+
+    // ——————————————————————————————————————————————————————————————————————//
+    io.to(IDs[counter % IDs.length].ID).emit("on turn");
+    //   console.log('im here -- 1');
+      
+    // } 
+
+    //
+    check_index++;
+    //
+
+
+    console.log('im here -- 2');
+    
+    //second stage
+    if (check_index == (IDs.length -fold_list.length) && stage ==1){
+
+      // print pot and flop message
+      io.emit('chat message', "current pot is:  " + total_pot);
+
+      //display the flop to the screen
+      io.emit('display', flop[0], 0);
+      io.emit('display', flop[1], 1);
+      io.emit('display', flop[2], 2);
+
+      stage = 2
+      check_index = 0
+      //update stage to client
+      io.emit('stage check', stage);
+    }
+
+    //third stage
+    if (check_index == (IDs.length -fold_list.length) && stage ==2){
+
+      //print total pot and turn
+      io.emit('chat message', "current pot is:  " + total_pot);
+      // io.emit('chat message','Turn' + [flop[0], flop[1], flop[2], turn[0]].join());
+
+      //display the turn on screen
+      io.emit('display', turn[0], 3);
+
+
+      stage = 3
+      check_index = 0;
+      //update stage to client
+      io.emit('stage check', stage);
+    }
+
+    //fourth stage
+    if (check_index == (IDs.length -fold_list.length) && stage ==3){
+      //print current pot and river 
+      io.emit('chat message', "current pot is:  " + total_pot);
+      //display the river on screen
+      io.emit('display', river[0], 4);
+
+
+      stage = 4
+      check_index = 0;
+      //update stage to client
+      io.emit('stage check', stage);
+    }
+
+    //fifth stage-- 
+    //1.who to send money --- not solved
+    //2.ready for new game
+    if(check_index == (IDs.length -fold_list.length) && stage == 4){
+      //reset everything
+      resetOnServer();
+      io.emit('reset game');
+    }
+    
+  })
+
+
   // checking if all users r ready 
   socket.on('ready message', ()=> {
   ready_index++;
-  console.log(ready_index);
-
-  //check game start condition
+  
+  // check game start condition
   if (ready_index == IDs.length){
-    
-    if (stage == 0 && IDs.length == 2) {
+    if (stage == 0) {
       //shuffle the deck 
       temp = deck;
       shuffle(temp);
-      //send user card
-      first_hand[0] = temp[5]
-      first_hand[1] = temp[6]
-      second_hand[0] = temp[7]
-      second_hand[1] = temp[8]
+ 
       //send flip card 
-      flop[0] = temp[0]
-      flop[1] = temp[1]
-      flop[2] = temp[2]
-      turn[0] = temp[3]
-      river[0] = temp[4]
-      
-      //need to be fixed 
-      io.to(IDs[0]).emit('chat message','first user has: ' + [first_hand[0], first_hand[1]].join());
-      io.to(IDs[1]).emit('chat message','second user has: ' + [second_hand[0], second_hand[1]].join());
-      // io.to(IDs[IDs.length-1]).emit('chat message', 'blah blah blah' + IDs[IDs.length-1])
+      flop[0] = temp[47]
+      flop[1] = temp[48]
+      flop[2] = temp[49]
+      turn[0] = temp[50]
+      river[0] = temp[51]
 
+      for (let i = 0; i < IDs.length; i++){
+        IDs[i].setHand(temp[2*i],temp[2*i+1]);
+        // send first card to user
+        io.to(IDs[i].ID).emit('display',IDs[i].firstCard, 5);
+        // send second card to user
+        io.to(IDs[i].ID).emit('display',IDs[i].secondCard, 6);
+      }
       stage = 1
+      io.emit('stage check', stage);
+      io.emit('game start');
+      if(!fold_list.includes(IDs[counter % IDs.length].ID)){
+        io.to(IDs[counter % IDs.length].ID).emit("on turn");
+      }
 
-    } 
-  }
-  
-  // //second stage
-  // if (msg === 'a'){
-  //   console.log('im here1111')
-  //   check_index++;
-  //   if (check_index == 2 && stage ==1) {
-  //     io.emit('chat message','Flop: ' + [flop[0], flop[1], flop[2]].join());
-  //     stage = 2
-  //     check_index = 0
-  //   }
-    
-  // }
-
-  // //third stage
-  // if (msg === 'b'){
-  //   console.log('im here2222')
-  //   check_index++;
-  //   if (check_index == 2 && stage ==2) {
-  //     io.emit('chat message','Turn' + [flop[0], flop[1], flop[2], turn[0]].join());
-  //     stage = 3
-  //     check_index = 0
-  //   }
-  // }
-
-  // //fourth stage
-  // if (msg === 'c'){
-  //   console.log('im here3333')
-  //   check_index++;
-  //   if (check_index == 2 && stage == 3){
-  //     io.emit('chat message','River' + [flop[0], flop[1], flop[2], turn[0], river[0]].join());
-  //     stage = 4
-  //     check_index = 0
-  //   }
-  // }
-
-
-})
+    }}
+  })
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
+
+  //calculate the bet and print
+  socket.on('get bet', (arg)=> {
+    io.emit('chat message', 'User bet: ' +arg+'  bet  ' +arg+ '  to follow');
+    total_pot = total_pot + Number(arg);
+  })
 });
 
 
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-// io.on('connection', (socket) => {
-//   socket.broadcast.emit('hi');
-// });
-
-
-
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
@@ -143,4 +202,15 @@ function shuffle(array)  {
   array.sort(() => Math.random() - 0.5)
 }
 
-
+function resetOnServer() {
+  check_index = 0;
+  ready_index = 0;
+  stage =0;
+  flop = ['', '', '']
+  turn = ['']
+  river = ['']
+  temp =[];
+  total_pot =0;
+  counter = 0;
+  fold_list = [];
+}
